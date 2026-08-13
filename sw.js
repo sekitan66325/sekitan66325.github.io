b2c2e4c58128226761f1651fe48c61c1c9a2346a
せきたん <sekitan.info@gmail.com>
2026-08-13 11:12:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,6 +7,11 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  // GAS (script.google.com) への通信は Service Worker で処理せず、ブラウザ標準の通信に任せる
+  if (event.request.url.includes('script.google.com')) {
+    return;
+  }
+
   // 通常のネットワークリクエストをそのまま通過させる
   event.respondWith(fetch(event.request));
 });
