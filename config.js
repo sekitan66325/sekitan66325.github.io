c989c6b1c2958bec565a83dc57a4defbb20b71c6
せきたん <sekitan.info@gmail.com>
2026-08-25 18:15:04 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-const CONFIG = {
-  GAS_API_URL: "https://script.google.com/macros/s/AKfycbyqXXN9JTOBk8u6rPy7zFWnrxHN3GO9r6BI5ociKDKXLMwKfcMp1AXlbxjawelPw0g/exec"
+﻿const CONFIG = {
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbyqXXN9JTOBk8u6rPy7zFWnrxHN3GO9r6BI5ociKDKXLMwKfcMp1AXlbxjawelPw0g/exec",
+  STATUS_API_URL: "https://script.google.com/macros/s/AKfycbytI6W8gN_iKiRdnruiyDxggxKItUMCNA5KkPIkLtEhqgZxkmkOIOeLHxHO4OneRdrZ5g/exec"
 };
